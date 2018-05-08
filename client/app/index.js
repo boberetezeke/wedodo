dbfdc6ed1e5d17d40e7599ee35dd2c20895b2e35
Steve Tuckner <stevetuckner@gmail.com>
2018-05-08 17:53:09 -0500

    add back in redux

--- client/app/index.js
+++ client/app/index.js
@@ -8,23 +8,33 @@ import {
   Switch
 } from 'react-router-dom'
 
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import App from './components/App/App';
 import NotFound from './components/App/NotFound';
 
 import Home from './components/Home/Home';
 
 import HelloWorld from './components/HelloWorld/HelloWorld';
+import Login from './components/Home/Login';
+import { rootReducer } from "../store/rootReducer";
 
 import './styles/styles.scss';
 
+const store = createStore(rootReducer)
+
 render((
   <Router>
-    <App>
-      <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route path="/helloworld" component={HelloWorld}/>
-        <Route component={NotFound}/>
-      </Switch>
-    </App>
+    <Provider store={store}>
+        <App>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/helloworld" component={HelloWorld}/>
+            <Route path="/login" component={Login}/>
+            <Route component={NotFound}/>
+          </Switch>
+        </App>
+    </Provider>
   </Router>
 ), document.getElementById('app'));
